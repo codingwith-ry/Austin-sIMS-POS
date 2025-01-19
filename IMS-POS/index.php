80c7140d8b1a82752d3b1b193a49c5340a6848a7
Stephen Hao <stephenlance.hao.cics@ust.edu.ph>
2025-01-19 15:49:15 +0800

    New file footer.php additional changes to index.php to test if verticalNav.php and footer.php are working

--- IMS-POS/index.php
+++ IMS-POS/index.php
@@ -1,11 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    
-</body>
-</html>
+<?php include 'verticalNav.php'?>
+<main>
+    <h1>Main Page</h1>
+</main>
+<?php include 'footer.php' ?>
--- IMS-POS/verticalNav.php
+++ IMS-POS/verticalNav.php
@@ -15,7 +15,7 @@
     </div>
         <div class="offcanvas-body">
         <ul class="list-group list-group-flush">
-            <li class="list-group-item"><a href="Dashboard.php">Dashboard</a></li>
+            <li class="list-group-item">Dashboard</li>
             <li class="list-group-item">A second item</li>
             <li class="list-group-item">A third item</li>
             <li class="list-group-item">A fourth item</li>
