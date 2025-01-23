b8d8336360546a359a789d48ef421717feb4a886
Stephen Hao <stephenlance.hao.cics@ust.edu.ph>
2025-01-23 15:49:56 +0800

    linked nav.css in verticalnav

--- IMS-POS/verticalNav.php
+++ IMS-POS/verticalNav.php
@@ -18,7 +18,7 @@
     <link rel="stylesheet" href="https://cdn.datatables.net/responsive/3.0.3/css/responsive.bootstrap5.css">
     <link rel="stylesheet" href="https://cdn.datatables.net/2.2.1/css/dataTables.dataTables.css">
     <link rel="stylesheet" href="https://cdn.datatables.net/buttons/3.2.0/css/buttons.bootstrap5.css">
-    <link rel="stylesheet" type="text/css" href="nav.css">
+    <link rel="stylesheet" type="text/css" href="styles/nav.css">
     <link href="styles/POS.css" rel="stylesheet">
     <link href="styles/IMS.CSS" rel="stylesheet">
     
