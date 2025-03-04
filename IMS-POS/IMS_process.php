afdb4559ad0b70089ac7696a8433e589adc0f10f
Stephen Hao <stephenlance.hao.cics@ust.edu.ph>
2025-03-04 16:23:33 +0800

    changes made to the add item form

--- IMS-POS/IMS_process.php
+++ IMS-POS/IMS_process.php
@@ -8,12 +8,26 @@ try {
     throw new PDOException($e->getMessage(), (int)$e->getCode());
 }
 
+/* Fetching Category List from the database */
 $fetchCategoryQuery = "SELECT * FROM tbl_categories";
 $categories = $pdo->query($fetchCategoryQuery)->fetchAll(PDO::FETCH_ASSOC);
-?>
 
-<ul>
-    <?php foreach ($categories as $category): ?>
-        <li><?php echo htmlspecialchars($category['categoryName']); ?></li>
-    <?php endforeach; ?>
-</ul>
+/* Fetching Employee List from the database */
+$fetchEmployeeQuery = "SELECT * FROM employees";
+$employee = $pdo -> query($fetchEmployeeQuery) -> fetchAll(PDO::FETCH_ASSOC);
+
+/* Pushing inventory data to the database */
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+    if(isset($_POST["item_name"]) && isset($_POST["item_category"]) && isset($_POST["item_price"]) 
+    && isset($_POST["purchase_date"]) && isset($_POST["expiration_date"]) && isset($_POST["employee_assigned"])){
+        $item_Name = $_POST["item_name"];
+        $item_Category = $_POST["item_category"];
+        $item_Price = $_POST["item_price"];
+        $purchase_date = $_POST["purchase_date"];
+        $expiration_date = $_POST["expiration_date"];
+        $employee_assigned = $_POST["employee_assigned"];
+        header("Location: IMS.php");
+
+    }
+}
+?>
